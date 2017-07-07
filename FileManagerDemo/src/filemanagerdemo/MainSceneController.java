8f3d8575d460dea1816336b9598c56595be1ea88
Artem Novickiy <work-for-me-here@yandex.ru>
2017-07-07 22:02:51 +0300

    add control pane with control button, moving a window

---
+++ FileManagerDemo/src/filemanagerdemo/MainSceneController.java
@@ -0,0 +1,67 @@
+package filemanagerdemo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.effect.Bloom;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author fakelake
+ */
+public class MainSceneController implements Initializable {
+    private Stage stage;
+    
+    @FXML
+    private Pane controlPane;
+    @FXML
+    private Button btnClose;
+    @FXML
+    private Button btnResize;
+    @FXML
+    private Button btnHide;
+    
+    @FXML
+    private void handleButtonClose(ActionEvent event) {
+        stage.close();
+    }
+    
+    @FXML
+    private void handleButtonHide(ActionEvent event) {
+        stage.setIconified(true);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Перемещение окна  
+        Coordinates dragDelta = new Coordinates();
+        
+        controlPane.setOnMousePressed((MouseEvent mouseEvent) -> {
+            dragDelta.x = stage.getX() - mouseEvent.getScreenX();
+            dragDelta.y = stage.getY() - mouseEvent.getScreenY();
+        });
+        controlPane.setOnMouseDragged((MouseEvent mouseEvent) -> {
+            stage.setX(mouseEvent.getScreenX() + dragDelta.x);
+            stage.setY(mouseEvent.getScreenY() + dragDelta.y);
+        });
+        
+        btnClose.setEffect(new Bloom());
+        btnResize.setEffect(new Bloom());
+        btnHide.setEffect(new Bloom());
+    }
+
+    void setStage(Stage primaryStage) {
+        this.stage = primaryStage;
+    }
+
+    private static class Coordinates {
+        public double x;
+        public double y;
+    }
+}
